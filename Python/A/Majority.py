004c9d822c5bf66efb1ad604941a5f3f6a3f9b04
github <github>
2024-07-17 17:20:29 +0900

    Add Python File

---
+++ Python/A/Majority.py
@@ -0,0 +1,18 @@
+N = int(input())
+
+FCount = 0
+ACount = 0
+
+for _ in range(N):
+    temp = input()
+    if temp == "For":
+        FCount += 1
+    else:
+        ACount += 1
+        
+if FCount >= ACount:
+    print("Yes")
+else:
+    print("No")
+
+
